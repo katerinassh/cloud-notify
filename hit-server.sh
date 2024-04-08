ec245313ff6a5dcfb598db40042038b63fd178fa
@KXS1310 <kxs1310@yum.com>
2024-04-08 13:42:57 +0300

    Add script for cron job

---
+++ hit-server.sh
@@ -0,0 +1,8 @@
+# URL to hit
+url="http://13.48.1.69:3000"
+
+# Make the HTTP request using curl
+response=$(curl -s -o /dev/null -w "%{http_code}" $url)
+
+# Print the response code
+echo "Response Status Code: $response"
